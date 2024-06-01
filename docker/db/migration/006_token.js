fdf5874bf97cd289a7bce482ea1d4cc70955174d
Abdul <abdul.hasib08@gmail.com>
2024-06-01 15:26:09 +0100

    token table added expires_at

--- docker/db/migration/006_token.js
+++ docker/db/migration/006_token.js
@@ -2,7 +2,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('token', function(table) {
     table.increments('id').primary();
     table.integer('user_id').unsigned().notNullable().references('id').inTable('user').onDelete('CASCADE');
-    table.string('refresh_token').notNullable(); // Assuming you store refresh tokens
+    table.string('refresh_token').notNullable();
+    table.timestamp('expires_at').notNullable();
     table.timestamps(true, true);
   });
 };
